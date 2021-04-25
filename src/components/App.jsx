b6616f172a5861029e5a1272db389d1664ec43b9
Angela Caterina Gallo <72492654+angelacaterina@users.noreply.github.com>
2021-04-25 20:44:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,9 +12,3 @@ function App() {
 }
 
 export default App;
-
-/** Component.jsx
- * importare React e ulteriori componenti interni
- * funzione che restituisce il componente
- * esportare il componente
- */
