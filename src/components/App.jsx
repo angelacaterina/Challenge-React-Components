256c7f402a782f1dc96fe4abb4891f044a84801a
Angela Caterina Gallo <72492654+angelacaterina@users.noreply.github.com>
2021-04-25 20:38:51 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
+
+/** Component.jsx
+ * importare React e ulteriori componenti interni
+ * funzione che restituisce il componente
+ * esportare il componente
+ */
